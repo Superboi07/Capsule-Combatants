7932261fcd4aad3ccd84bc8c659f6ebdac6d0969
Superboi777 <68790401+Superboi07@users.noreply.github.com>
2026-04-13 15:32:00 +0000

    folder adjustment and basic setup

---
+++ Main/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Main
+{
+    // main logic class
+    class Program
+    {
+        // initzation method; for use untill switching to actual devcade IDE
+        // run via console: [dotnet run --project Main]
+        public static void Main(String[]  args)
+        {
+            Initalize();
+        }
+
+        // does things which need to happen before loading content
+        static void Initalize()
+        {
+            LoadContent;
+        }
+
+        // loads content
+        static void LoadContent()
+        {
+            Update;
+        }
+
+        // primary update loop
+        static void Update()
+        {
+            string line = Console.ReadLine();
+
+            if (line == "test")
+            {
+                return;
+            }
+
+            Update();
+        }
+    }
+
+    // handles individual units
+    class Unit
+    {
+        public String name;
+        public Unit(String name)
+        {
+            this.name = name;
+        }
+    }
+}
